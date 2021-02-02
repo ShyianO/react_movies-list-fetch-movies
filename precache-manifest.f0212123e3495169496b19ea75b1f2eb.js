6c095fc78de308ea29c113e4569741b1b38defb5
ShyianO <ol.shyian@gmacil.com>
2021-02-02 19:36:32 +0200

    Updates

---
+++ precache-manifest.f0212123e3495169496b19ea75b1f2eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a63509bdb05c4e62648f8ff75a71e01",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3975b71121c00e7e430c",
+    "url": "./static/css/2.9634ee28.chunk.css"
+  },
+  {
+    "revision": "99887fb43aea585b9695",
+    "url": "./static/css/main.6ce43aae.chunk.css"
+  },
+  {
+    "revision": "3975b71121c00e7e430c",
+    "url": "./static/js/2.089ad005.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "./static/js/2.089ad005.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99887fb43aea585b9695",
+    "url": "./static/js/main.6d3b49b8.chunk.js"
+  },
+  {
+    "revision": "aae0386031766cdb5520",
+    "url": "./static/js/runtime-main.63136dd4.js"
+  }
+]);
